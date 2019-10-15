4adfa373cadc07f3e097daf3c16fb3d3e9048b49
Norman Moeschter <norman.moeschter@gmail.com>
2019-10-15 11:52:46 +0200

    Use get_most_recent_tag.py as module.

--- get_most_recent_tag.py
+++ get_most_recent_tag.py
@@ -27,6 +27,7 @@ How to:
 
 PRIVATE_TOKEN = os.environ.get("GITLAB_PRIVATE_TOKEN", None)
 GROUP_ID = os.environ.get("GITLAB_GROUP_ID", None)
+URL = os.environ.get("GITLAB_URL", None)
 
 GITHUB_API_ENDPOINT = "/api/v4"
 ISSUES_ENDPOINT = "/issues"
@@ -34,49 +35,67 @@ PROJECT_ENDPOINT = "/projects" + "/{project_id}"
 TAGS_ENDPOINT = "/repository/tags"
 PROJECT_TAGS_ENDPOINT = f"{PROJECT_ENDPOINT}" + f"{TAGS_ENDPOINT}"
 
-parser = argparse.ArgumentParser(description='Get most recent tags in group repositories.', epilog="python get_most_recent_tag.py --token $(pass show work/CSS/gitlab/private_token) --url <gitlab_url> --group <gitlab_group_id> --latest", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-u', '--url', required=True, default="https://example.gitlab.com", help='Gitlab host/url/server.')
-parser.add_argument('-g', '--group', required=True, default="1", help='Gitlab group id.')
-parser.add_argument('-t', '--token', nargs='?', help='Private Token to access gitlab API. If not given as argument, set GITLAB_PRIVATE_TOKEN.')
-# default=False is implied by action='store_true'
-parser.add_argument('-l', '--latest', action='store_true', help='Show most recent tag only.')
-args = parser.parse_args()
+def get_most_recent_tags(url=URL, group_id=GROUP_ID, headers=None, latest_only=False):
+    url = url + GITHUB_API_ENDPOINT
 
-if not PRIVATE_TOKEN:
-    PRIVATE_TOKEN = args.token
-if not GROUP_ID:
-    GROUP_ID = args.group
-LATEST_ONLY = args.latest
-
-URL = args.url + GITHUB_API_ENDPOINT
-
-headers = {"PRIVATE-TOKEN": PRIVATE_TOKEN}
-
-def get_most_recent_tags(group_id=-1):
     group_endpoint = f"/groups/{group_id}/projects"
-    response = requests.get(URL + f"{group_endpoint}", headers=headers)
+    print(f"{url + group_endpoint}")
+    response = requests.get(url + f"{group_endpoint}", headers=headers)
     if not response.status_code == 200:
         print(f"Received status code {response.status_code} with {response.text}")
-        sys.exit
+        sys.exit(1)
     projects = response.json()
 
     projects_tags = defaultdict(list)
     for project in projects:
         project_id = project.get("id", None)
         project_endpoint = PROJECT_TAGS_ENDPOINT.format(project_id=project_id)
-        response = requests.get(URL + project_endpoint, headers=headers)
+        response = requests.get(url + project_endpoint, headers=headers)
         if not response.status_code == 200:
             print(f"Received status code {response.status_code} with {response.text}")
             sys.exit
         tags = response.json()
         for tag in tags:
             projects_tags[project.get("name", None)].append(tag.get("name", None))
-            if LATEST_ONLY:
+            if latest_only:
                 break
     return projects_tags
 
-if __name__ == "__main__":
-    tags = get_most_recent_tags(group_id=GROUP_ID)
+
+def main():
+    parser = argparse.ArgumentParser(description='Get most recent tags in group repositories.', epilog="python get_most_recent_tag.py --token $(pass show work/CSS/gitlab/private_token) --url <gitlab_url> --group <gitlab_group_id> --latest", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-u', '--url', required=True, default="https://example.gitlab.com", help='Gitlab host/url/server.')
+    parser.add_argument('-g', '--group', required=True, default="-1", help='Gitlab group id.')
+    parser.add_argument('-t', '--token', nargs='?', help='Private Token to access gitlab API. If not given as argument, set GITLAB_PRIVATE_TOKEN.')
+    # default=False is implied by action='store_true'
+    parser.add_argument('-l', '--latest', action='store_true', help='Show most recent tag only.')
+    args = parser.parse_args()
+
+
+    if PRIVATE_TOKEN:
+        private_token = PRIVATE_TOKEN
+    else:
+        private_token = args.token
+
+    if GROUP_ID:
+        group_id = GROUP_ID
+    else:
+        group_id = args.group
+
+    latest_only = args.latest
+
+    if URL:
+        url = URL
+    else:
+        url = args.url
+
+    headers = {"PRIVATE-TOKEN": private_token}
+
+    tags = get_most_recent_tags(url=url, group_id=group_id, headers=headers, latest_only=latest_only)
     for k, v in tags.items():
         print(k)
         print("  " +  "\n  ".join(t for t in v))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
